575861b3f3455431fe027c4ceb97fe4a63a66dd7
francoluca35 <francolucap1@gmail.com>
2025-09-02 00:18:28 -0300

    .

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? {
-      cssnano: {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: true,
-          colormin: true,
-          minifyFontValues: true,
-          minifySelectors: true,
-        }],
-      },
-    } : {}),
-  },
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
 }
